aa84194647c71cce543576174f468471952c80b9
Rachel S. Lee <rslee0025@gmail.com>
2020-06-08 02:03:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,6 @@ class Song
    genre_count = Hash.new(0) #sets default value for any key to 0
    @@genres.each { |genre| genre_count[genre] += 1 } 
    genre_count
-   binding.pry
  end 
  
  def self.artist_count
