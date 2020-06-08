596e75689af3a149255a0e75e23ac6a1f33b4566
Rachel S. Lee <rslee0025@gmail.com>
2020-06-08 01:27:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,34 +2,19 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0 
-  @@genres = []
-  @@artists = []
-  
+ 
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist 
     @genre = genre 
     @@count += 1
-    @@genres = genres
-    @@artists = artists
+  
   end 
   
   def self.count
     @@count 
   end
   
-  def self.genres
-    @@genres << genre
-  end
-  
-  def self.artists
-    @@artists << artist
-  end 
-  
-  def self.genre_count
-  end 
-  
-  def self.artist_count
-  end
+ 
 end 
 
